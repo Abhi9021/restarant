91f8502d98a827fb4460ddebd696f76c19e7f5c2
Abhi9021 <104748269+Abhi9021@users.noreply.github.com>
2022-07-26 08:40:19 +0530

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,16 @@
+let menu=document.querySelector('#menu-bar');
+let navbar=document.querySelector('.navbar');
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   loop:true,
+  });
